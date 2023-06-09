ebc280e8614f3f335223c0a883149befd3453eb2
jesper-sk <47450353+JesperK123456@users.noreply.github.com>
2023-06-09 19:29:31 +0200

    Fixed the setuptools thing

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
-    name='ewiser',
-    version='0.1',
-    packages=['ewiser'],
-    url='',
-    license='',
-    author='Michele Bevilacqua',
-    author_email='bevilacqua@di.uniroma1.it',
-    description='Enhanced WSD Integrating Synset Embeddings and Relations'
+    name="ewiser",
+    version="0.1",
+    packages=find_packages(),
+    url="",
+    license="",
+    author="Michele Bevilacqua",
+    author_email="bevilacqua@di.uniroma1.it",
+    description="Enhanced WSD Integrating Synset Embeddings and Relations",
 )
